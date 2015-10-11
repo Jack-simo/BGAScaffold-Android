5954a60f89297c955406f7d840016289aafce2ae
bingoogolapple <bingoogolapple@gmail.com>
2015-10-11 21:05:05 +0800

    完成删除闹钟

--- alarmclock/src/main/java/cn/bingoogolapple/alarmclock/ui/activity/MainActivity.java
+++ alarmclock/src/main/java/cn/bingoogolapple/alarmclock/ui/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,16 +15,18 @@ import java.util.List;
 import cn.bingoogolapple.alarmclock.R;
 import cn.bingoogolapple.alarmclock.dao.PlanDao;
 import cn.bingoogolapple.alarmclock.model.Plan;
+import cn.bingoogolapple.androidcommon.adapter.BGAOnItemChildCheckedChangeListener;
 import cn.bingoogolapple.androidcommon.adapter.BGAOnItemChildClickListener;
 import cn.bingoogolapple.androidcommon.adapter.BGARecyclerViewAdapter;
 import cn.bingoogolapple.androidcommon.adapter.BGAViewHolderHelper;
 import cn.bingoogolapple.basenote.activity.BaseActivity;
 import cn.bingoogolapple.basenote.util.CalendarUtil;
+import cn.bingoogolapple.basenote.util.ToastUtil;
 import cn.bingoogolapple.basenote.widget.Divider;
 import cn.bingoogolapple.swipeitemlayout.BGASwipeItemLayout;
 import cn.bingoogolapple.titlebar.BGATitlebar;
 
-public class MainActivity extends BaseActivity implements BGAOnItemChildClickListener {
+public class MainActivity extends BaseActivity implements BGAOnItemChildClickListener, BGAOnItemChildCheckedChangeListener {
     private static final int REQUEST_CODE_ADD = 1;
     private static final int REQUEST_CODE_VIEW = 2;
     private RecyclerView mDataRv;
@@ -48,6 +51,7 @@ public class MainActivity extends BaseActivity implements BGAOnItemChildClickLis
 
         mPlanAdapter = new PlanAdapter(mDataRv);
         mPlanAdapter.setOnItemChildClickListener(this);
+        mPlanAdapter.setOnItemChildCheckedChangeListener(this);
 
         mDataRv.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
@@ -75,14 +79,20 @@ public class MainActivity extends BaseActivity implements BGAOnItemChildClickLis
     @Override
     public void onItemChildClick(ViewGroup parent, View childView, int position) {
         if (childView.getId() == R.id.tv_item_plan_delete) {
-            mPlanAdapter.closeOpenedSwipeItemLayoutWithAnim();
-            mPlanAdapter.removeItem(position);
+            deletePlan(position);
         } else if (childView.getId() == R.id.rl_item_plan_container) {
             mCurrentViewPosition = position;
             Intent intent = new Intent(this, DetailActivity.class);
             intent.putExtra(DetailActivity.EXTRA_OPERATE_TYPE, DetailActivity.OPERATE_TYPE_VIEW);
             intent.putExtra(DetailActivity.EXTRA_PLAN, mPlanAdapter.getItem(position));
             forward(intent, REQUEST_CODE_VIEW);
+        }
+    }
+
+    @Override
+    public void onItemChildCheckedChanged(ViewGroup parent, CompoundButton childView, int position, boolean isChecked) {
+        if (!mPlanAdapter.isIgnoreChange()) {
+            updatePlanStatus(position);
         }
     }
 
@@ -136,19 +146,92 @@ public class MainActivity extends BaseActivity implements BGAOnItemChildClickLis
         }.execute();
     }
 
+    private void updatePlanStatus(final int position) {
+        final Plan plan = mPlanAdapter.getItem(position);
+        final int newStatus = plan.status == Plan.STATUS_ALREADY_HANDLE ? Plan.STATUS_NOT_HANDLE : Plan.STATUS_ALREADY_HANDLE;
+        new AsyncTask<Void, Void, Boolean>() {
+            @Override
+            protected void onPreExecute() {
+                showLoadingDialog(R.string.loading);
+            }
+
+            @Override
+            protected Boolean doInBackground(Void... params) {
+                long beginTime = System.currentTimeMillis();
+                boolean result = PlanDao.updatePlan(plan.id, plan.time, plan.content, newStatus);
+                long time = System.currentTimeMillis() - beginTime;
+                if (time < DELAY_TIME) {
+                    try {
+                        Thread.sleep(DELAY_TIME - time);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(Boolean result) {
+                dismissLoadingDialog();
+                if (result) {
+                    plan.status = newStatus;
+                    mPlanAdapter.notifyItemChanged(position);
+                } else {
+                    ToastUtil.show(R.string.toast_update_plan_failure);
+                }
+            }
+        }.execute();
+    }
+
+    private void deletePlan(final int position) {
+        new AsyncTask<Void, Void, Boolean>() {
+            @Override
+            protected void onPreExecute() {
+                showLoadingDialog(R.string.loading);
+            }
+
+            @Override
+            protected Boolean doInBackground(Void... params) {
+                long beginTime = System.currentTimeMillis();
+                boolean result = PlanDao.deletePlan(mPlanAdapter.getItem(position).id);
+                long time = System.currentTimeMillis() - beginTime;
+                if (time < DELAY_TIME) {
+                    try {
+                        Thread.sleep(DELAY_TIME - time);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(Boolean result) {
+                dismissLoadingDialog();
+                if (result) {
+                    mPlanAdapter.closeOpenedSwipeItemLayoutWithAnim();
+                    mPlanAdapter.removeItem(position);
+                } else {
+                    ToastUtil.show(R.string.toast_delete_plan_failure);
+                }
+            }
+        }.execute();
+    }
+
     private final class PlanAdapter extends BGARecyclerViewAdapter<Plan> {
         /**
          * 当前处于打开状态的item
          */
         private List<BGASwipeItemLayout> mOpenedSil = new ArrayList<>();
+        private boolean mIsIgnoreChange = false;
 
         public PlanAdapter(RecyclerView recyclerView) {
             super(recyclerView, R.layout.item_plan);
         }
 
         @Override
-        protected void setItemChildListener(BGAViewHolderHelper viewHolderHelper) {
-            BGASwipeItemLayout swipeItemLayout = viewHolderHelper.getView(R.id.sil_item_plan_root);
+        protected void setItemChildListener(BGAViewHolderHelper helper) {
+            BGASwipeItemLayout swipeItemLayout = helper.getView(R.id.sil_item_plan_root);
             swipeItemLayout.setDelegate(new BGASwipeItemLayout.BGASwipeItemLayoutDelegate() {
                 @Override
                 public void onBGASwipeItemLayoutOpened(BGASwipeItemLayout swipeItemLayout) {
@@ -166,14 +249,19 @@ public class MainActivity extends BaseActivity implements BGAOnItemChildClickLis
                     closeOpenedSwipeItemLayoutWithAnim();
                 }
             });
-            viewHolderHelper.setItemChildClickListener(R.id.rl_item_plan_container);
-            viewHolderHelper.setItemChildClickListener(R.id.tv_item_plan_delete);
+            helper.setItemChildClickListener(R.id.rl_item_plan_container);
+            helper.setItemChildClickListener(R.id.tv_item_plan_delete);
+            helper.setItemChildCheckedChangeListener(R.id.switch_item_plan_status);
         }
 
         @Override
         protected void fillData(BGAViewHolderHelper helper, int position, Plan model) {
             helper.setText(R.id.tv_item_plan_content, model.content);
             helper.setText(R.id.tv_item_plan_time, CalendarUtil.formatDisplayTime(model.time));
+
+            mIsIgnoreChange = true;
+            helper.setChecked(R.id.switch_item_plan_status, model.status == Plan.STATUS_ALREADY_HANDLE);
+            mIsIgnoreChange = false;
         }
 
         public void closeOpenedSwipeItemLayoutWithAnim() {
@@ -181,6 +269,10 @@ public class MainActivity extends BaseActivity implements BGAOnItemChildClickLis
                 sil.closeWithAnim();
             }
             mOpenedSil.clear();
+        }
+
+        public boolean isIgnoreChange() {
+            return mIsIgnoreChange;
         }
     }
 
