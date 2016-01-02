86c7e1f9204cbd8bba49cec788a3a8c82d26a713
bingoogolapple <bingoogolapple@gmail.com>
2016-01-03 07:19:42 +0800

    修改操作符学习笔记

--- rxjava/src/main/java/cn/bingoogolapple/rxjava/ui/activity/OperatorActivity.java
+++ rxjava/src/main/java/cn/bingoogolapple/rxjava/ui/activity/OperatorActivity.java
@@ -5,14 +5,19 @@ import android.view.View;
 
 import com.trello.rxlifecycle.ActivityEvent;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import cn.bingoogolapple.basenote.activity.TitlebarActivity;
 import cn.bingoogolapple.basenote.util.Logger;
 import cn.bingoogolapple.rxjava.R;
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
+import rx.functions.Action1;
 import rx.functions.Func0;
+import rx.functions.Func1;
+import rx.functions.Func2;
 
 /**
  * 作者:王浩 邮件:bingoogolapple@gmail.com
@@ -108,4 +113,130 @@ public class OperatorActivity extends TitlebarActivity {
         // 演示3秒发送
         Observable.timer(3, TimeUnit.SECONDS).compose(bindUntilEvent(ActivityEvent.PAUSE)).subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
     }
+
+    public void take(View v) {
+        // 选取前3个，不足3个时有多少个就输出多少个
+        Observable.just(1, 2, 3, 4, 5).take(3).subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void takeFirst(View v) {
+        // 选取第一个符合条件的输出
+        Observable.just(1, 2, 3, 4, 5).takeFirst(new Func1<Integer, Boolean>() {
+            @Override
+            public Boolean call(Integer integer) {
+                return integer % 2 == 0;
+            }
+        }).subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void takeLast(View v) {
+        // 选取最后3个，不足3个时有多少个就输出多少个
+        Observable.just(1, 2, 3, 4, 5).takeLast(7).subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void takeLastBuffer(View v) {
+        // 去最后2个，并转换成集合，takeLastBuffer <==> takeLast(count).toList()
+        Observable.just(1, 2, 3, 4, 5).takeLastBuffer(2).subscribe(new Action1<List<Integer>>() {
+            @Override
+            public void call(List<Integer> integers) {
+                Logger.i(TAG, integers.toString());
+            }
+        });
+    }
+
+    public void takeUntil(View v) {
+        // 一直取，知道integer为4
+        Observable.just(1, 2, 3, 4, 5).takeUntil(new Func1<Integer, Boolean>() {
+            @Override
+            public Boolean call(Integer integer) {
+                return integer == 4;
+            }
+        }).subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void takeWhile(View v) {
+        // 当integer小于4的时候一直取
+        Observable.just(1, 2, 3, 4, 5).takeWhile(new Func1<Integer, Boolean>() {
+            @Override
+            public Boolean call(Integer integer) {
+                return integer < 4;
+            }
+        }).subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void distinct(View v) {
+        Observable.just(1, 2, 3, 2, 3, 5).distinct().subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void first(View v) {
+        Observable.just(1, 2, 3, 2, 3, 5).first().subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void last(View v) {
+        Observable.just(1, 2, 3, 2, 3, 5).last().subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void skip(View v) {
+        Observable.just(1, 2, 3, 4, 5, 6).skip(2).subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void skipLast(View v) {
+        Observable.just(1, 2, 3, 2, 3, 5).skipLast(2).subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void skipWhile(View v) {
+        // 有一次不满足条件时开始打印，即使后面再有满足条件的，还是会照常打印
+        Observable.just(1, 2, 3, 2, 3, 5).skipWhile(new Func1<Integer, Boolean>() {
+            @Override
+            public Boolean call(Integer integer) {
+                return integer < 3;
+            }
+        }).subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void elementAt(View v) {
+        Observable.just(1, 2, 3, 4, 5, 6).elementAt(7).subscribe(new Observer<Integer>() {
+            @Override
+            public void onCompleted() {
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Logger.e(TAG, "错误" + e.getMessage());
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Logger.i(TAG, String.valueOf(integer));
+            }
+        });
+    }
+
+    public void elementAtOrDefault(View v) {
+        Observable.just(1, 2, 3, 4, 5, 6).elementAtOrDefault(7, 10).subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void switchMap(View v) {
+        // concatMap()函数解决了flatMap()的交叉问题，提供了一种能够把发射的值连在一起的铺平函数，而不是合并它们
+        Observable.just(1, 2, 3, 4, 5, 6).switchMap(new Func1<Integer, Observable<String>>() {
+            @Override
+            public Observable<String> call(Integer integer) {
+                return Observable.just(String.valueOf(integer));
+            }
+        }).subscribe(s -> Logger.i(TAG, s));
+    }
+
+    public void scan(View v) {
+        Observable.just(1, 2, 3, 4, 5, 6).scan(new Func2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) {
+                return integer + integer2;
+            }
+        }).subscribe(integer -> Logger.i(TAG, String.valueOf(integer)));
+    }
+
+    public void buffer(View v) {
+        Observable.just(1, 2, 3, 4, 5, 6).buffer(3).subscribe(list -> Logger.i(TAG, String.valueOf(list)));
+    }
+
 }
