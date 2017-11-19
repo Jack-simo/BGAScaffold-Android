6b87eb41f33dee500119f77d61f6842ca545e8fd
bingoogolapple <bingoogolapple@gmail.com>
2017-11-20 03:04:25 +0800

    完成吸顶分类搜索

--- demo/src/main/java/cn/bingoogolapple/scaffolding/demo/rxjava/activity/StickySearchActivity.java
+++ demo/src/main/java/cn/bingoogolapple/scaffolding/demo/rxjava/activity/StickySearchActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.SearchView;
 import com.orhanobut.logger.Logger;
 
 import java.net.ConnectException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -20,9 +19,8 @@ import cn.bingoogolapple.scaffolding.demo.R;
 import cn.bingoogolapple.scaffolding.demo.rxjava.api.Engine;
 import cn.bingoogolapple.scaffolding.demo.rxjava.entity.Blog;
 import cn.bingoogolapple.scaffolding.demo.rxjava.entity.Category;
-import cn.bingoogolapple.scaffolding.util.NetResult;
+import cn.bingoogolapple.scaffolding.util.CollectionUtil;
 import cn.bingoogolapple.scaffolding.util.StringUtil;
-import cn.bingoogolapple.scaffolding.util.ToastUtil;
 import cn.bingoogolapple.scaffolding.view.MvcActivity;
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
@@ -45,7 +43,7 @@ public class StickySearchActivity extends MvcActivity {
 
     private Engine.BlogApi mBlogApi;
     private LongSparseArray<String> mCategoryArray = new LongSparseArray<>();
-    private List<Category> mCategoryList = new ArrayList<>();
+    private List<Category> mCategoryList;
 
     @Override
     protected int getRootLayoutResID() {
@@ -68,9 +66,8 @@ public class StickySearchActivity extends MvcActivity {
 
             @Override
             public boolean onQueryTextChange(String keyword) {
-                if (mKeywordPs == null) {
-                    initSearch();
-                }
+//                initSearch2();
+
                 mKeywordPs.onNext(keyword);
                 return true;
             }
@@ -81,6 +78,7 @@ public class StickySearchActivity extends MvcActivity {
     protected void processLogic(Bundle savedInstanceState) {
         mBlogApi = Engine.getRxJavaApi();
         initStickyList();
+        initSearch();
     }
 
     // 初始化吸顶效果列表
@@ -109,17 +107,77 @@ public class StickySearchActivity extends MvcActivity {
         mBlogRv.setAdapter(mBlogAdapter);
     }
 
-    // 初始化搜索
     private void initSearch() {
+        mKeywordPs = PublishSubject.create();
+        mKeywordPs.debounce(400, TimeUnit.MILLISECONDS) // debounce 默认是在 computation 线程的。发送一个延时消息给下游，如果在这段延时时间内没有收到新的请求，那么下游就会收到该消息；而如果在这段延时时间内收到来新的请求，那么就会取消之前的消息，并重新发送一个新的延时消息
+                .observeOn(AndroidSchedulers.mainThread()) // 这里手动将后续操作符切换到主线程，否则 filter 也是在 computation 线程的
+                .filter(keyword -> { // 只有返回 true 时，才会将事件发送给下游，否则就丢弃该事件
+                    if (StringUtil.isNotEmpty(keyword)) {
+                        return true;
+                    } else {
+                        mBlogAdapter.clear();
+                        return false;
+                    }
+                })
+                .switchMap(keyword -> { // switchMap 将上游的事件转换成新的 Observable，如果在该节点收到一个新的事件之后，那么如果之前收到的事件所产生的 Observable A 还没有发送事件给下游，那么下游就再也不会收到 Observable A
+                    /**
+                     * CombineLatest 和 zip 类似，都是组合两个 Observable 的数据为新的 Observable
+                     * zip 当原始 Observable 中每一个都发射了一条数据时才发射数据
+                     * CombineLatest 当原始 Observable 中任何一个发射了一条数据时发射数据
+                     */
+                    return Observable.combineLatest(
+                            getCategoryListObservable(),
+                            mBlogApi.findBlogList(keyword).flatMap(netResult -> Observable.just(netResult.data)),
+                            (blogCategoryList, blogList) -> {
+                                convertToCategoryArray(blogCategoryList);
+                                return blogList;
+                            }
+                    );
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .compose(bindToLifecycle())
+                .doOnError(throwable -> {
+                    throwable.printStackTrace();
+                    Logger.d("错误：" + throwable.getMessage()); // 将 Observer 的 onError 中的错误处理放到 doOnError 中处理
+                })
+                .retryWhen(throwableObservable -> throwableObservable) // 处理 onError 时重订阅，避免发生一次错误后就再也搜索不到结果。Observer 的 onError 将不会再被回调
+                .filter(result -> StringUtil.isNotEmpty(mKeywordSv.getQuery())) // 避免返回结果时，如果当前搜索框关键字为空则忽略此次搜索结果
+                .subscribe(result -> {
+                    Logger.d("查询成功");
+                    mBlogAdapter.setData(result);
+                });
+    }
+
+    // 初始化搜索
+    private void initSearch2() {
+        if (mKeywordPs != null) {
+            return;
+        }
+        mKeywordPs = PublishSubject.create();
         /**
          * CombineLatest 和 zip 类似，都是组合两个 Observable 的数据为新的 Observable
          * zip 当原始 Observable 中每一个都发射了一条数据时才发射数据
          * CombineLatest 当原始 Observable 中任何一个发射了一条数据时发射数据
          */
-        Observable.combineLatest(getCategoryListObservable(), getBlogListObservable(), (blogCategoryList, blogList) -> {
-            convertToCategoryArray(blogCategoryList);
-            return blogList;
-        }).observeOn(AndroidSchedulers.mainThread())
+        Observable.combineLatest(
+                getCategoryListObservable(),
+                mKeywordPs.debounce(400, TimeUnit.MILLISECONDS) // debounce 默认是在 computation 线程的。发送一个延时消息给下游，如果在这段延时时间内没有收到新的请求，那么下游就会收到该消息；而如果在这段延时时间内收到来新的请求，那么就会取消之前的消息，并重新发送一个新的延时消息
+                        .observeOn(AndroidSchedulers.mainThread()) // 这里手动将后续操作符切换到主线程，否则 filter 也是在 computation 线程的
+                        .filter(keyword -> { // 只有返回 true 时，才会将事件发送给下游，否则就丢弃该事件
+                            if (StringUtil.isNotEmpty(keyword)) {
+                                return true;
+                            } else {
+                                mBlogAdapter.clear();
+                                return false;
+                            }
+                        })
+                        .switchMap(keyword -> mBlogApi.findBlogList(keyword) // switchMap 将上游的事件转换成新的 Observable，如果在该节点收到一个新的事件之后，那么如果之前收到的事件所产生的 Observable A 还没有发送事件给下游，那么下游就再也不会收到 Observable A
+                                .flatMap(netResult -> Observable.just(netResult.data))),
+                (blogCategoryList, blogList) -> {
+                    convertToCategoryArray(blogCategoryList);
+                    return blogList;
+                })
+                .observeOn(AndroidSchedulers.mainThread())
                 .compose(bindToLifecycle())
                 .retryWhen(new Function<Observable<Throwable>, ObservableSource<?>>() {
                     private int mRetryCount;
@@ -138,6 +196,7 @@ public class StickySearchActivity extends MvcActivity {
                                 }
                             } else {
                                 Logger.d("未知异常");
+                                throwable.printStackTrace();
                                 return Observable.error(throwable);
                             }
                         });
@@ -147,47 +206,39 @@ public class StickySearchActivity extends MvcActivity {
                 .subscribe(result -> {
                     mBlogAdapter.setData(result);
                 }, throwable -> {
-                    mKeywordPs = null;
+                    mKeywordPs = null; // 收到错误时将 mKeywordPs，以便下次 keyword 发生变化重新订阅
                     Logger.d("错误：" + throwable.getMessage());
-                    ToastUtil.show("错误：" + throwable.getMessage());
                 });
-    }
-
-    // 获取发送博客列表的被观察者
-    private Observable<List<Blog>> getBlogListObservable() {
-        mKeywordPs = PublishSubject.create();
-        return mKeywordPs.debounce(400, TimeUnit.MILLISECONDS) // debounce 默认是在 computation 线程的。发送一个延时消息给下游，如果在这段延时时间内没有收到新的请求，那么下游就会收到该消息；而如果在这段延时时间内收到来新的请求，那么就会取消之前的消息，并重新发送一个新的延时消息
-                .observeOn(AndroidSchedulers.mainThread()) // 这里手动将后续操作符切换到主线程，否则 filter 也是在 computation 线程的
-                .filter(keyword -> { // 只有返回 true 时，才会将事件发送给下游，否则就丢弃该事件
-                    if (StringUtil.isNotEmpty(keyword)) {
-                        return true;
-                    } else {
-                        mBlogAdapter.clear();
-                        return false;
-                    }
-                }).switchMap(keyword -> mBlogApi.findBlogList(keyword)
-                        .flatMap(new Function<NetResult<List<Blog>>, ObservableSource<List<Blog>>>() {
-                            @Override
-                            public ObservableSource<List<Blog>> apply(NetResult<List<Blog>> netResult) throws Exception {
-                                return Observable.just(netResult.data);
-                            }
-                        }));
     }
 
     // 获取发送分类列表的被观察者
     private Observable<List<Category>> getCategoryListObservable() {
-        // concat 操作符是接收若干个 Observables，发射数据是有序的，不会交叉。concat + takeUntil 实现二级缓存
-        return Observable.concat(
-                Observable.just(mCategoryList),
-                mBlogApi.getCategoryList().flatMap(netResult -> Observable.just(netResult.data))
-        ).takeUntil(categoryList -> categoryList != null && mCategoryList.size() > 0)
+        // switchIfEmpty 如果原始 Observable 正常终止后仍然没有发射任何数据，就使用备用的 Observable
+        return getCategoryListFromCache()
+                .switchIfEmpty(mBlogApi.getCategoryList().flatMap(netResult -> Observable.just(netResult.data)))
                 .doOnNext(categoryList -> convertToCategoryArray(categoryList));
+
+        // concat 操作符是接收若干个 Observables，发射数据是有序的，不会交叉。concat + takeUntil 实现二级缓存
+//        return Observable.concat(
+//                getCategoryListFromCache(),
+//                mBlogApi.getCategoryList().flatMap(netResult -> Observable.just(netResult.data))
+//        ).takeUntil(categoryList -> categoryList != null)
+//                .doOnNext(categoryList -> convertToCategoryArray(categoryList));
+    }
+
+    // 从缓存中获取分类列表
+    private Observable<List<Category>> getCategoryListFromCache() {
+        // defer 只有当订阅者订阅时才创建 Observable，为每个订阅者创建一个新的 Observable。内部通过 ObservableDefer 在订阅时调用 Callable 的 call 方法创建 Observable
+        return Observable.defer(() -> {
+            Logger.d("defer");
+            return CollectionUtil.isEmpty(mCategoryList) ? Observable.empty() : Observable.just(mCategoryList);
+        });
     }
 
     // 将分类集合转成 id 和名称映射的 LongSparseArray
     private void convertToCategoryArray(List<Category> categoryList) {
         // 只有当 mCategoryList 为空时才转换
-        if (mCategoryList.size() == 0) {
+        if (CollectionUtil.isEmpty(mCategoryList)) {
             mCategoryArray.clear();
             for (Category category : categoryList) {
                 mCategoryArray.put(category.getId(), category.getName());
